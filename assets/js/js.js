2f2f1f50194e0354d4abb6728895bd628c3420a8
beks2995 <beks2992@gmail.com>
2019-07-08 18:39:02 +0600

    new commit

---
+++ assets/js/js.js
@@ -0,0 +1,14 @@
+$("#slider").owlCarousel({
+    navigation: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    autoHeight: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        2000: {
+            items: 1
+        }
+    }
+});
